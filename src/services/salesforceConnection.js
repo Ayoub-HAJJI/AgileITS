748d74cb1a3051a85990a0247c4b691ea9fd74cd
Ayoub HAJJI <ayoubhajjii94@gmail.com>
2023-07-18 17:24:09 +0100

    First Commit

--- src/services/salesforceConnection.js
+++ src/services/salesforceConnection.js
@@ -2,6 +2,6 @@ import { Connection } from 'jsforce';
 
 export const salesforceConnection = new Connection({
   instanceUrl:'https://process-knowledge-14683.my.salesforce.com',
-  accessToken: '00DHo0000016uPb!AQcAQGeRQYJu8slZqb8dOtf4Yrdw1o.P9OApH_cDd1_yIcPfBx9_x_0wGbyVS0tNuTZMuCR7NgTSBCOz8Bx_a0E4p1NzO_es'
+  accessToken: '00DHo0000016uPb!AQcAQHYwd5vGuSSoYkSWUdKcf77UziAKpY.hMC9k4gPiNns.qD3wMzbfjAVPw8n9.jbwyExCc0xMbfddCEzhFHbaeEfJlL0T'
 
 });
---
+++ src/store/actions/userSF.js
@@ -0,0 +1,58 @@
+import { userService2 } from "../../services";
+import { createAction } from ".";
+import { actionType } from "./type";
+import { notifitying } from "../../utils/notification";
+
+export const fetchAllUsers = (params) => {
+  return async (dispatch) => {
+    try {
+      const users = await userService2.fetchAllUsers(params);
+
+      dispatch(createAction(actionType.SET_USER_LIST, users));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const deleteUser = (id) => {
+  return async (dispatch) => {
+    try {
+      await userService2.deleterUser(id);
+
+      dispatch(fetchAllUsers());
+
+      notifitying("success", "User successfully deleted");
+    } catch (err) {
+      console.log(err);
+      notifitying("warning", "User failed to be deleted");
+    }
+  };
+};
+
+export const getMembersByProjectId = (id) => {
+  return async (dispatch) => {
+    try {
+      const members = await userService2.getMembersByProjectId(id);
+
+      dispatch(createAction(actionType.GET_PROJECT_MEMBERS, members));
+    } catch (err) {
+      dispatch(createAction(actionType.GET_PROJECT_MEMBERS, []));
+      console.log(err);
+    }
+  };
+};
+
+export const updateUser = (data, callback) => {
+  return async (dispatch) => {
+    try {
+      await userService2.updateUser(data);
+
+      if (callback) {
+        callback();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
